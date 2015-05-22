1b0e8266896d05be1b1036e867cc8e37a278b303
Ruben Polo <polouters@gmail.com>
2015-05-22 13:05:47 +0200

    jaba doc echo

--- proyecto/src/bd/SorteoBD.java
+++ proyecto/src/bd/SorteoBD.java
@@ -21,7 +21,10 @@ import java.sql.Connection;
 public class SorteoBD {
     
    private static Connection conn;
-  
+/**
+ * 
+ * @throws Exception 
+ */  
     public static void RealizarSorteo() throws Exception{
        /**
         * realiza el sorteo mediante PL/SQL
